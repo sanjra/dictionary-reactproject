c24341781d430c139c6326355bfe7d84fb244a62
sanjra <sanjrabezzi@gmail.com>
2022-03-25 19:06:02 +1100

    Bootstrap check

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App</header>
+      <header className="App-header">
+        Dictionary App
+        <a href="#" className="btn btn-primary">
+          Don't Push Me
+        </a>
+      </header>
     </div>
   );
 }
