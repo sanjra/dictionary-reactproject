073e049f6a706c55b44251f6fffcc3d3bc65646b
sanjra <sanjrabezzi@gmail.com>
2022-03-28 06:33:18 +1100

    dictionary api added.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-
+    // api documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
