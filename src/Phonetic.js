320b6f225f21aea1e00775050007e7c0dd9bbeb6
sanjra <sanjrabezzi@gmail.com>
2022-04-02 17:00:30 +1100

    Phonetics ADJ

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,9 +4,8 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen ►
-      </a>
-      <br />
+        Listen
+      </a>{" "}
       {props.phonetic.text}
     </div>
   );
